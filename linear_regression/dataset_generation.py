56674b9259ab99bf766c7e008eaed645cfee3adf
Luc Blassel <luc.blassel@gmail.com>
2020-01-08 18:20:46 +0100

    added gradient

--- linear_regression/dataset_generation.py
+++ linear_regression/dataset_generation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 def get_diffs(X, y_, coef):
     ymin, ymax = [], []
@@ -8,19 +9,20 @@ def get_diffs(X, y_, coef):
         ymax.append(max(x * coef, y))
     return ymin, ymax
 
-def plot_dataset(X, y, coef):
-    plt.scatter(X, y)
-    plt.plot(sorted(X), [coef * x for x in sorted(X)], linestyle=':', label=f"y = {coef}*x")
-    plt.legend()
-    plt.xlabel('x')
-    plt.ylabel('y')
+def plot_dataset(X, y, coef, ax):
+    ax.scatter(X, y, color='blue')
+    ax.plot(sorted(X), [coef * x for x in sorted(X)], linestyle=':', label=f"y = {coef}*x", color='red')
+    ax.legend()
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
 
-def plot_diffs(X, y, coef):
+def plot_diffs(X, y, coef, ax):
     ymin, ymax = get_diffs(X, y, coef)
-    plt.vlines(X, ymin, ymax, color='green')
-    plot_dataset(X, y, coef)
+    plot_dataset(X, y, coef, ax)
+    ax.vlines(X, ymin, ymax, color='green')
 
 if __name__ == "__main__":
+
     X = np.array(
         [0.25464312,  0.81562694, -1.64063088, -0.46683864,  2.09938726,
          0.26128934, -1.56805687, -0.89657469,  0.22647514,  1.2417255,
@@ -32,4 +34,38 @@ if __name__ == "__main__":
 
     coef = 2
 
-    
+    sns.set_context("poster")
+
+    # plot dataset
+    fig, ax = plt.subplots(1)
+    plot_dataset(X, y, coef, ax)
+    plt.show()
+
+    # show square distance
+    fig, axes = plt.subplots(1, 2, sharex=True, sharey=True)
+    plot_diffs(X, y, coef, axes[0])
+    plot_diffs(X, y, 3 * coef, axes[1])
+    plt.show()
+
+    # generate cost gradient showing
+    thetas, costs = list(range(-8, 13)), []
+    for theta in thetas: 
+        cost = sum(((X * theta) - y) ** 2) / (2 * len(y)) 
+        costs.append(cost)
+
+    plt.plot(thetas, costs)
+    plt.xticks(thetas)
+    plt.xlabel('theta')
+    plt.ylabel('Cost')
+    ymin, ymax = plt.ylim()
+    xmin, xmax = plt.xlim()
+    plt.hlines(y=min(costs), xmin=-100, xmax=2, linestyle=':', color='red')
+    plt.vlines(x=2, ymin=-100, ymax=min(costs), linestyle=':', color='red')
+    plt.ylim(ymin, ymax)
+    plt.xlim(xmin, xmax)
+    plt.annotate(
+        'minimal cost for\n theta=2', xy=(2,min(costs)),
+        xytext=(3, 7),
+        arrowprops=dict(facecolor='black', shrink=0.1, width=1),
+    )
+    plt.show()
